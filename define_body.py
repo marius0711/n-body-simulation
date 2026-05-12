0868734e0ed370d311b4955cd43038ffdd5d9d63
marius0711 <marius.zepf17@web.de>
2026-05-12 11:20:34 +0200

    chore: remove untracked files (define_body.py, duplicate chaos.png)

--- define_body.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-AU = 1.496e11
-pos_erde = np.array([AU, 0.0])
-vel_erde = np.array([0.0, 29_780])
-
-print("Position Erde:", pos_erde)
-print("Abstand zur Sonne:", np.linalg.norm(pos_erde), "m")
-print("Geschwindigkeit:", np.linalg.norm(vel_erde), "m/s")
